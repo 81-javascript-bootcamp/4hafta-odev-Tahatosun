08048c7226a8d9ad9dbefcd740ef796bc7c8e6a5
Tahatosun <35890316+Tahatosun@users.noreply.github.com>
2021-02-20 01:21:34 +0300

    Task Completed

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -5,3 +5,18 @@ export const searchMovieByTitle = (movie, searchValue) => {
 export const makeBgActive = (movie) => {
     document.querySelector(`tr[data-id='${movie.id}']`).style.background = "#d7f0f7";
 }
+
+/* Birden fazla genre ile filtreleme yapıldığında tabloda bulunan her genre için random bir arka plan rengi atar
+let tempGenre;
+let randomColor = "";
+
+export const makeBgRandom = (movie) => {
+
+    if(tempGenre != movie.genre){
+         randomColor = Math.floor(Math.random()*16777215).toString(16);
+    }   
+
+    document.querySelector(`tr[data-id='${movie.id}']`).style.background = `#${randomColor}`;
+    tempGenre=movie.genre;
+}
+*/
--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,124 @@
 import data from "./data.js";
-import {searchMovieByTitle, makeBgActive} from "./helpers.js";
+import { searchMovieByTitle, makeBgActive/*, makeBgRandom*/ } from "./helpers.js";
 
 class MoviesApp {
     constructor(options) {
-        const {root, searchInput, searchForm, yearHandler, yearSubmitter} = options;
+        const { root, searchInput, searchForm, yearHandler, yearSubmitter, yearFilter, genreFilter,genreSubmitter } = options;
         this.$tableEl = document.getElementById(root);
         this.$tbodyEl = this.$tableEl.querySelector("tbody");
 
         this.$searchInput = document.getElementById(searchInput);
-        this.$searchForm   = document.getElementById(searchForm);
+        this.$searchForm = document.getElementById(searchForm);
         this.yearHandler = yearHandler;
         this.$yearSubmitter = document.getElementById(yearSubmitter);
+        this.$yearFilter = document.getElementById(yearFilter);
+
+        this.$genreFilter = document.getElementById(genreFilter);
+        this.$genreSubmitter = document.getElementById(genreSubmitter);
+
     }
 
-    createMovieEl(movie){
-        const {image, title, genre, year,id} = movie;
+    createMovieEl(movie) {
+        const { image, title, genre, year, id } = movie;
         return `<tr data-id="${id}"><td><img src="${image}"></td><td>${title}</td><td>${genre}</td><td>${year}</td></tr>`
     }
 
-    fillTable(){
+    createGenreEl(genre, count) {
+        return `<div class="form-check">
+                    <input class="form-check-input" name="genre" type="checkbox" value="${genre}" >
+                    <label class="form-check-label" for="flexCheckDefault">
+                    ${genre} (${count})
+                    </label>
+                </div>`
+    }
+    createYearEl(year, count) {
+        return ` <div class="form-check">
+                    <input class="form-check-input" type="radio" name="year" value="${year}">
+                    <label class="form-check-label" >
+                        ${year} (${count})
+                    </label>
+                </div>`
+    }
+    fillTable() {
         /* const moviesHTML = data.reduce((acc, cur) => {
             return acc + this.createMovieEl(cur);
         }, "");*/
         const moviesArr = data.map((movie) => {
-           return this.createMovieEl(movie)
+            return this.createMovieEl(movie)
         }).join("");
         this.$tbodyEl.innerHTML = moviesArr;
     }
 
-    reset(){
+    fillGenreFilter(genres) {
+        let genresKeys = Object.keys(genres);
+        const genresArr = genresKeys.map((genre) => {
+            return this.createGenreEl(genre, genres[genre])
+        }).join("");
+        this.$genreFilter.innerHTML += genresArr;
+    }
+
+    fillYearFilter(years) {
+        let yearKeys = Object.keys(years);
+        const yearsArr = yearKeys.map((year) => {
+            return this.createYearEl(year, years[year])
+        }).join("");
+        this.$yearFilter.innerHTML += yearsArr;
+    }
+
+
+    getGenres() {
+        let genres = [];
+        data.forEach((movie) => {
+            if (genres[movie.genre] != undefined) {
+                genres[movie.genre] += 1;
+            }
+            else {
+                genres[movie.genre] = 1;
+            }
+        });
+        this.fillGenreFilter(genres);
+    }
+
+
+    getYears() {
+        let years = [];
+        data.forEach((movie) => {
+            if (years[movie.year] != undefined) {
+                years[movie.year] += 1;
+            }
+            else {
+                years[movie.year] = 1;
+            }
+        });
+        this.fillYearFilter(years);
+    }
+
+
+    reset() {
         this.$tbodyEl.querySelectorAll("tr").forEach((item) => {
             item.style.background = "transparent";
         })
     }
 
 
-    handleSearch(){
+    handleSearch() {
         this.$searchForm.addEventListener("submit", (event) => {
+
             event.preventDefault();
             this.reset();
             const searchValue = this.$searchInput.value;
             const matchedMovies = data.filter((movie) => {
                 return searchMovieByTitle(movie, searchValue);
             }).forEach(makeBgActive)
-
+            this.$searchInput.value="";
         });
     }
 
-    handleYearFilter(){
+    handleYearFilter() {
+
         this.$yearSubmitter.addEventListener("click", () => {
+            document.getElementsByName(`genre`).forEach((genre)=> genre.checked=false);
+
             this.reset();
             const selectedYear = document.querySelector(`input[name='${this.yearHandler}']:checked`).value
             const matchedMovies = data.filter((movie) => {
@@ -57,10 +127,31 @@ class MoviesApp {
         });
     }
 
-    init(){
+    handleGenreFilter() {
+        this.$genreSubmitter.addEventListener("click", () => {
+            document.getElementsByName(`year`).forEach((year)=> year.checked=false);
+
+            this.reset();
+            const selectedGenres = document.getElementsByName(`genre`)
+            selectedGenres.forEach((genre)=>{
+                if(genre.checked){
+                    const matchedMovies = data.filter((movie) => {
+                        return movie.genre === genre.value;
+                    }).forEach(makeBgActive)
+                }
+               
+            })
+           
+        });
+    }
+
+    init() {
         this.fillTable();
         this.handleSearch();
         this.handleYearFilter();
+        this.handleGenreFilter();
+        this.getGenres();
+        this.getYears();
     }
 }
 
@@ -69,7 +160,10 @@ let myMoviesApp = new MoviesApp({
     searchInput: "searchInput",
     searchForm: "searchForm",
     yearHandler: "year",
-    yearSubmitter: "yearSubmitter"
+    yearSubmitter: "yearSubmitter",
+    genreFilter: "genre-filter",
+    yearFilter: "year-filter",
+    genreSubmitter:"genre-submitter"
 });
 
 myMoviesApp.init();
